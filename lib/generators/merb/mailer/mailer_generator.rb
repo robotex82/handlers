401ece1e2dd46713ec73b6f1bb9fa9f41a46f84b
Roberto Vasquez Angel <robotex@robotex.de>
2011-03-31 01:12:59 +0200

    Initial commit

---
+++ lib/generators/merb/mailer/mailer_generator.rb
@@ -0,0 +1,18 @@
+require "rails/generators/erb/mailer/mailer_generator"
+module Merb
+  module Generators
+    class MailerGenerator < Erb::Generators::MailerGenerator
+      source_root File.expand_path("../templates", __FILE__)
+      
+      protected
+        def format
+          nil # Our templates have no format
+        end
+        
+        def handler
+          :merb
+        end
+      # protected  
+    end
+  end
+end            
---
+++ test/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '143613230975a1fdf28098f06b34067006515e39cd07b5cf10033df9176eacbe3e843503e005dbc95ce86c2952c776fdb83003f777d155a574408bbe31c43e61'
